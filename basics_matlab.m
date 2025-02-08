8d577c22e5bc445976b1f34762e8dcdf7ed964a5
mwangelwa <mwangelwa@hotmail.com>
2025-02-08 10:39:26 +0300

    Resolved merge conflicts

---
+++ basics_matlab.m
@@ -0,0 +1,56 @@
+%% Data Types
+
+% Row Vectors 
+a = [1,2,3]
+
+% Column Vectr 
+b = [4;5;6]
+
+% Matrix 
+c = [1,2,3;4,5,6;7,8,9]
+
+%% Basic Mathematical Expressions 
+% - + * / ^
+
+x = 5
+
+% Exponential Function
+
+exp(x);
+
+
+% Pi
+
+pi
+
+% Log Function 
+
+log(x)
+
+% Power function 
+pow2(x) 
+
+% Trigonometric functions 
+
+y = 0;
+
+cos(y)
+
+sin(y)
+
+tan(y)
+
+% Rounding operation 
+
+z = 0;
+
+round(z)
+
+% Ceiling operation
+
+ceil(z)
+
+
+% floor operation
+
+floor(z)
