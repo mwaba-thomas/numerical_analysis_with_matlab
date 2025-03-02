1f858afbabc726b1f2e1784709f6e0e8d14f9839
mwangelwa <mwangelwa@hotmail.com>
2025-03-02 13:10:57 +0300

    new

---
+++ matlab_onramp_commands_save_and_load_variables.m
@@ -0,0 +1,14 @@
+%% Task 1
+% Create a variable named x with a value of π/2.
+
+x = pi / 2
+
+%% Task 2
+% Calculate the sine of x by using the sin function. Assign the result to a variable named y.
+
+y = sin(x)
+
+%% Task 3
+% Calculate the square root of -9 by using the sqrt function. Assign the result to a variable named z.
+
+z = sqrt(-9)
---
+++ matlab_onramp_commands_tasks.m
@@ -0,0 +1,12 @@
+%% Task 1
+
+% Create a variable named A with the value -2.
+
+A = -2 
+
+%% Task 2
+
+% Use A and a to calculate meanAa = (a+A)/2. then Store the result in a variable named meanAa.
+a = 8
+meanAa = (a + A) / 2 
+
